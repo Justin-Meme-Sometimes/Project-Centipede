18901c7a5024074ab336fc2dedfc9584755445ca
Ellie Oyebode <ellie.a.oyebode.26@dartmouth.edu>
2025-07-03 00:30:24 -0400

    adding password encryption

--- backend/models/userRoutes.js
+++ backend/models/userRoutes.js
@@ -1,12 +1,21 @@
 const express =  require('express');
 const router =  express.Router();
 const User =  require('../models/User');
+const bcrypt = require ('bcrypt');
 
 router.post('/', async (req, res) => {
     const { username, email, password } = req.body;
 
     try{
-        const newUser = new User({ username, email, password });
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({ 
+            username, 
+            email, 
+            password: hashedPassword,
+        });
+        
         await newUser.save();
         res.status(201).json({ message: 'User created!', user: newUser });
     } catch (err) {
